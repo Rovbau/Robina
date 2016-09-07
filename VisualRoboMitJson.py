2bb45a58d5f47648b47d513904769506a978b01c
Rovbau <rovbau@gmx.ch>
2016-09-07 21:29:01 +0200

    Visual mit LoadDialog und MouseOver Infos

--- VisualRoboMitJson.py
+++ VisualRoboMitJson.py
@@ -5,6 +5,7 @@ import json
 import socket
 from threading import Thread
 from Tkinter import *
+import tkFileDialog
 import time
 import pickle
 
@@ -17,6 +18,8 @@ root=Tk()
 root.title ("Hinderniss-Daten")           #Titel de Fensters
 root.geometry("700x700+0+0")
 can=Canvas(master=root, width=600, height=600, bg="grey")
+
+
 
 class Server():
     def __init__(self):
@@ -43,10 +46,10 @@ class Server():
         return()
 
     def getNewValues(self):
-        """Returns the data fromm the socket"""            
-        #self.xx = self.xx +10
-        #self.obs = [[1,1],[2,7],[2,8]]
-        #self.path = [[0,0],[40,40],[50,self.xx]]        
+        """Returns the data from the socket"""            
+        self.xx = self.xx +10
+        self.obs = [[1,1],[2,7],[2,8]]
+        self.path = [[0,0],[40,40],[50,self.xx]]        
         return(self.obs, self.path, self.solved_path1)
 
     def clearValues(self):
@@ -60,7 +63,7 @@ class Sichern():
         self.timeold = 0
 
     def storeFile(self, obst, path):
-        """Pickel Daten"""
+        """Pickel Daten: [Obstace][Path]"""
         filename = "Stube"        
         last_save = time.time()-self.timeold
         
@@ -70,9 +73,9 @@ class Sichern():
             pickelPath.close()
             self.timeold = time.time()
 
-    def loadFile(self):
-        filename = "Stube"
-        pickeln = open("LogRoboWeg-"+filename+".p", "rb")
+    def loadFile(self, filename):
+        """Un-pickle file and extract [Obstace][Path][Solved_Path]"""
+        pickeln = open (filename)
         data= pickle.load(pickeln)
         pickeln.close()
 
@@ -89,31 +92,33 @@ class Visual():
         self.position_in_grid = []
         self.solved_path = []
         self.sichern = sichern
+        self.flag_load_file=False
        
     def printObstacles(self):
         """Zeichne die Hindernisse und RoboPath"""
-
         Nullx = self.Nullx
         Nully = self.Nully
-        
-        obstacles, path, solved_path = serv.getNewValues()
-        #obstacles, path, solved_path = self.sichern.loadFile()
-        
-        print(obstacles[:10])
-        print(path[:10])
-        print(solved_path[:10])
 
-        if len(obstacles) != 0:
+        #Lade Json-Daten via Netz oder zeige File an
+        if self.flag_load_file == True:    
+            obstacles, path, solved_path = self.sichern.loadFile(self.file_name)
+            
+            self.obstacles_in_grid= obstacles
+            self.position_in_grid = path
+            self.solved_path = solved_path            
+        else:
+            obstacles, path, solved_path = serv.getNewValues()
+            serv.clearValues()
+            
             self.obstacles_in_grid.extend(obstacles)
             self.position_in_grid.extend(path)
             self.solved_path = solved_path
-        serv.clearValues()
+
+            #Save neu Points
+            self.sichern.storeFile(self.obstacles_in_grid, self.position_in_grid)
+
         can.delete("Point")
-
-        #Save neu Points
-        self.sichern.storeFile(self.obstacles_in_grid, self.position_in_grid)
-
-        print("DataPoints: "+str(len(self.obstacles_in_grid))+" "+str(len( self.position_in_grid)))
+        print("DataPoints [Obsta.][Pos.]: "+str(len(self.obstacles_in_grid))+" "+str(len( self.position_in_grid)))
         
         for pos in self.obstacles_in_grid:
             X=pos[0]
@@ -131,7 +136,7 @@ class Visual():
             for pos in self.solved_path:
                 X=pos[0]*10
                 Y=pos[1]*10
-                #Zeichne Hindernisspunkte Global ein 
+                #Zeichne A*-Wegpunkte Global ein 
                 can.create_oval(Nullx+X-3,Nully-Y+3,Nullx+X+3,Nully-Y-3, width=1, fill="green",tag="Point")
         except:
             can.create_oval(Nullx-3,Nully+3,Nullx+3,Nully-3, width=1, fill="blue",tag="Point")
@@ -151,7 +156,27 @@ class Visual():
         print("clear")
         self.obstacles_in_grid=[]
         self.position_in_grid=[]
-    
+
+    def getMousePos(self,event):
+        """Mousezeiger zeigt Pos der Hidernisse in Label an"""
+        point_id = can.find_closest(event.x,event.y)
+        item_color = can. itemcget(point_id, "fill")
+        if item_color == "red":
+            entryIP.delete(0,40)
+            entryIP.insert(0,"Hinderniss bei X: "+str(event.x-self.Nullx)+" "+"Y: "+str(event.y-self.Nully))
+        else:   
+            entryIP.delete(0,40)
+            entryIP.insert(0,"Koordinate bei X: "+str(event.x-self.Nullx)+" "+"Y: "+str(event.y-self.Nully))
+        return()
+
+    def loaden(self):
+        """Oeffnet Dialogfenster um Datei zu laden"""
+        self.flag_load_file = False
+        self.file_name = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",
+                                                      filetypes = (("pickels","*.p"),("all files","*.*")))
+        self.flag_load_file = True
+        root.title (self.file_name)
+        
 ###MAIN###
 
 serv = Server()
@@ -171,16 +196,24 @@ buttonL = Button(root, text="Links", fg="blue",command=lambda: visual.setZoom(-1
 buttonR = Button(root, text="Rechts", fg="blue",command=lambda: visual.setZoom(100,0))
 buttonU = Button(root, text="Unten", fg="blue",command=lambda: visual.setZoom(0,100))
 buttonO = Button(root, text="Oben", fg="blue",command=lambda: visual.setZoom(0,-100))
+buttonMenu = Button(root, text="Load", fg="blue",command=visual.loaden)
 buttonClear = Button(root, text="Clear", fg="blue",command=visual.clearPoints)
+
 buttonO.pack(side=TOP)
-buttonClear.pack(side=TOP)
+buttonMenu.pack()
+buttonClear.pack()
 buttonU.pack(side=BOTTOM)
 buttonR.pack(side=RIGHT)
 buttonL.pack(side=LEFT)
 
-entryIP = Entry(root)
+entryIP = Entry(root, width= 30)
 entryIP.pack()
-entryIP.insert(10,"Ring entspricht 50 cm")
+entryIP.insert(0,"Ring entspricht 50 cm")
+
+label = Label(root)
+label.pack()
+can.bind("<Button-1>",visual.getMousePos)
+
 can.create_oval(Nullx-2,Nully+2,Nullx+2,Nully-2, width=1, fill=None)
 can.create_oval(Nullx-50,Nully+50,Nullx+50,Nully-50, width=1, fill=None, outline="gray78")
 can.create_oval(Nullx-100,Nully+100,Nullx+100,Nully-100, width=1, fill=None, outline="gray78")
