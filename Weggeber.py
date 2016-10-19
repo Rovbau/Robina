4fd1bee2bd2596b138627c2394cd29699f6a524a
Michael von Burg <michi.vonburg@gmx.ch>
2016-10-19 14:49:10 +0200

    I2c-Ic fhelr bei Clear-Register behoben. Neu counts bis 900 then clears

--- Weggeber.py
+++ Weggeber.py
@@ -15,7 +15,11 @@ class Weggeber():
         self.PortLRueck=33
         self.PortRRueck=29
 
-        self.DiffCount=0
+        self.DiffCount = 0
+        self.counts_left_old = 0
+        self.counts_right_old = 0
+        self.CountL = 0
+        self.CountR = 0
        
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.PortLRueck,GPIO.OUT)
@@ -37,33 +41,44 @@ class Weggeber():
 
     def runAllTime(self):
         """Count pulse L und R und DistanzRad, loop"""
-        self.CountL=0
-        self.CountR=0
 
         #Get Weggeber counts and clear Register
-        left = bus.read_byte_data(self.addrweggeberL,0x01)
+        left = bus.read_word_data(self.addrweggeberL,0x01)
         countsL = int(hex(left).replace('0x', ''))          #BCD-Code to Int
         
-        right = bus.read_byte_data(self.addrweggeberR,0x01)
+        right = bus.read_word_data(self.addrweggeberR,0x01)        
         countsR = int(hex(right).replace('0x', ''))         #BCD-Code to Int
-                        
+
+        aktualL_counts = countsL - self.counts_left_old 
+        aktualR_counts = countsR - self.counts_right_old
+
+        self.counts_left_old = countsL
+        self.counts_right_old = countsR
+        
+        if countsL > 900 or countsR > 900:
+            bus.write_word_data(self.addrweggeberL,0x01,0x00)       
+            bus.write_word_data(self.addrweggeberR,0x01,0x00)            
+
+            self.counts_left_old = 0
+            self.counts_right_old = 0
+
+
+        #Count minus wenn Robo retour                       
         if GPIO.input(self.PortLRueck)==1:
-            self.CountL = countsL * (-1)
+            self.CountL = self.CountL + aktualL_counts * (-1)
         else:
-            self.CountL = countsL
+            self.CountL = self.CountL + aktualL_counts
         
         if GPIO.input(self.PortRRueck)==1:
-            self.CountR = countsR * (-1)
+            self.CountR = self.CountR + aktualR_counts * (-1)
         else:
-            self.CountR = countsR
+            self.CountR = self.CountR + aktualR_counts
 
         self.DiffCount=self.CountR-self.CountL
 
     
     def clearWeggeberLR(self):
         """clears L / R Counts"""
-        bus.write_byte_data(self.addrweggeberL,0x01,0x00)       
-        bus.write_byte_data(self.addrweggeberR,0x01,0x00)
         self.DiffCount=0
         self.CountR=0
         self.CountL=0
@@ -81,15 +96,11 @@ if __name__ == "__main__":
     from threading import *
     print("Starte")
     weggeber=Weggeber()
-    ThreadEncoder=Thread(target=weggeber.runAllTime,args=())
-    ThreadEncoder.daemon=True
-    ThreadEncoder.start()
 
-    sleep(0.3)
-    
     while True:
-        print(str(weggeber.getPulseLR()))
-        sleep(1)
+        weggeber.runAllTime()
+        print(weggeber.getPulseLR())
+        sleep(0.21)
 
 
     
