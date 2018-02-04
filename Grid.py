406e1f5093faf0457846b2ce8c5825bd84c5186a
Rovbau <rovbau@gmx.ch>
2018-02-04 18:04:34 +0100

    Grid wird mit probalistik Karte berechnet. Hindernisse +10 leere -1

--- Grid.py
+++ Grid.py
@@ -1,43 +1,95 @@
+#! python
 #Erzeugt Grid Basics
 
 #([(0, 50),(10, 50),(20, 50),(30, 50),(40, 50),(90, 90)])
 
 from copy import deepcopy
 import cPickle as pickle
-from Astar import *
+#from Astar import *
+import math
+import numpy as np
+import sys
 
 class Grid():
     def __init__(self, width, heigh):
-        self.width=width
-        self.heigh=heigh
-        self.walls=[[1,2,3,],[2,3,4]]
-        self.gridwithweights=GridWithWeights(width,heigh)
-        self.clearance_add_walls=[]
-        self.last_start_pos=(width,heigh)
-        
+        self.width = width
+        self.heigh = heigh
+        self.walls = []
+        #self.gridwithweights=GridWithWeights(width,heigh)
+        self.clearance_add_walls = []
+        self.last_start_pos = (width,heigh)
+        self.karten_arr = np.zeros(shape = (self.width, self.heigh))
+        self.x_middle = self.width / 2
+        self.y_middle = self.heigh / 2         
         
     def obstaclesInGrid(self, obstacles):
         """GlobaleHinderniss (x[cm],y[cm]) in Grid eintragen (RasterX,RasterY)"""        
         unpaintedObstacles=obstacles
         self.roundet_walls=[]
+        neg_element = []
         
         for obstacle in unpaintedObstacles:
-            element_allready = False
+            
             #round to 5 Bsp: x=int(round(obstacle[0]/5.0)*5.0)
             obstacle_x_grid = int(obstacle[0]/10)
             obstacle_y_grid = int(obstacle[1]/10)
+            obstacle_x_grid = obstacle_x_grid + self.x_middle
+            obstacle_y_grid = obstacle_y_grid + self.y_middle
+            x_occ_pre = obstacle_x_grid
+            y_occ_pre = obstacle_y_grid
+
+#************************
+            x_start, y_start = self.startgrid
+            x_start = x_start + self.x_middle
+            y_start = y_start + self.y_middle
+            hinderniss = self.karten_arr[obstacle_x_grid, obstacle_y_grid] + 10 #Add Value zu Hinderniss
+            self.karten_arr[obstacle_x_grid, obstacle_y_grid] = hinderniss
             
-            for element in self.walls:
-                if element[0:2] == [obstacle_x_grid,obstacle_y_grid]:
-                    element[2] = element[2]+1
-                    self.roundet_walls.append(element)
-                    element_allready = True
-                    break
-            if element_allready == False:
-                    self.walls.append([obstacle_x_grid,obstacle_y_grid,1])
-                    self.gridwithweights.walls=deepcopy(self.walls)
-                    self.roundet_walls.append([obstacle_x_grid,obstacle_y_grid,1])
-                                        
+            self.check_for_double(obstacle_x_grid, obstacle_y_grid)                         #Keine doppel
+            
+            print("start" +str(x_start))
+            dist, winkel = self.cart2pol(obstacle_x_grid -  x_start, obstacle_y_grid - y_start)
+            print(dist, winkel)
+
+            for distance in range(0, int(round(dist))):                                 #Von dist=0 bis zum Hinderniss
+                x_occ, y_occ = self.pol2cart(distance, winkel)
+                print(x_occ, y_occ)
+                
+                if x_occ != x_occ_pre or  y_occ != y_occ_pre:
+                    self.karten_arr[x_occ + x_start ,y_occ + y_start] = self.karten_arr[x_occ + x_start ,y_occ + y_start] - 1     #Reduziere Feldwert da kein Hinderniss
+                    x_occ_pre, y_occ_pre = x_occ, y_occ
+
+                    self.check_for_double(x_occ + x_start ,y_occ + y_start)                                 #Keine doppel in roundet_walls
+            
+        print(self.karten_arr)
+        print(sys.getsizeof(self.karten_arr))
+        print(sys.getsizeof(self.roundet_walls))
+        
+    def check_for_double(self,x_occ, y_occ):
+        x_occ = x_occ - self.x_middle
+        y_occ = y_occ - self.y_middle
+        element_allready = False
+        for element in self.roundet_walls:                                  #Keine doppel in roundet_walls
+            element_allready = False
+            if element[0:2] == [x_occ, y_occ]:
+                element[2] = self.karten_arr[x_occ, y_occ]
+                element_allready = True
+                break                           
+        if element_allready == False:                                       #Add neue Felder zu roundet_walls
+            self.walls.append([x_occ, y_occ,1])
+            #self.gridwithweights.walls=deepcopy(self.walls)
+            self.roundet_walls.append([x_occ, y_occ, self.karten_arr[x_occ + self.x_middle, y_occ + self.x_middle]])
+
+    def cart2pol(self, x, y):
+        dist = math.sqrt(x**2 + y**2)
+        winkel = math.atan2(y, x)
+        return(dist, winkel)
+
+    def pol2cart(self, dist, winkel):
+        x = dist * math.cos(winkel)
+        y = dist * math.sin(winkel)
+        return(int(round(x)), int(round(y)))
+                                   
     def getRoundetWalls(self):
         return(self.roundet_walls)
 
@@ -103,12 +155,12 @@ class Grid():
 
         print("SUCHE...Weg")
 
-        motor.setCommand(0,0)
+        #motor.setCommand(0,0)
         #print(self.gridwithweights.walls)
         #No Wall on Robo-Ist position
-        if self.startgrid in self.gridwithweights.walls:
-            self.gridwithweights.walls.remove(self.startgrid)
-            print("Del StartPosi in Grid")
+        #if self.startgrid in self.gridwithweights.walls:
+        #    self.gridwithweights.walls.remove(self.startgrid)
+         #   print("Del StartPosi in Grid")
             
         #No Wall on Robo-Ziel position
         if self.zielgrid in self.gridwithweights.walls:
@@ -144,16 +196,17 @@ class Grid():
 
 if __name__ == "__main__":
 
-    g = Grid(100,100)
-    g.setStartInGrid(20,20)
+    g = Grid(12,12)
+    g.setStartInGrid(30,30)
     g.setZielInGrid(80,80)
 
-    g.obstaclesInGrid([[70, 80],[90, 80],[80, 70],[80, 90]])    
+    g.obstaclesInGrid([[-50, -50]])    #,[80, 80],[80, 70],[80, 90]
+
     #g.addClearance()
-    print(g.walls)
+    print(sorted(g.getRoundetWalls()))
     print("***")
-    weg=g.getSolvedPath(1,1,1)
-    print(weg)
-    g.drawSolvedPath()
-    g.saveGridPath(weg)
+    #weg=g.getSolvedPath(1,1,1)
+    #print(weg)
+    #g.drawSolvedPath()
+    #g.saveGridPath(weg)
 
